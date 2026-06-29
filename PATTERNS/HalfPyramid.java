4816f57f09db4efb2bdb5e783690b35b020a46e2
Amarjeet Kushwaha <amarjeet62063@gmail.com>
2026-06-29 17:33:33 +0530

    PATTERN

--- PATTERNS/HalfPyramid.java
+++ PATTERNS/HalfPyramid.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class HalfPyramid {
   public static void main(String[]args){
      Scanner sc =new Scanner(System.in);
-    int n=5;
+    int n=sc.nextInt();
 
     for (int i = 0; i < n; i++) {
           //outer loop
-          
+
           for (int j = 1; j <n-i; j++) {
             // inner loop for space
 
@@ -18,7 +18,7 @@ public class HalfPyramid {
 
           for (int j = 0; j < 2*i+1; j++) {
              //inner loop for star
-
+             
             System.out.print("*");
           }
           System.out.println();
